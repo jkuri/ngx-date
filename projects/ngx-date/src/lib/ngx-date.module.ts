9509c5bb5cbe0bd204e3bdf075d13d491701d726
Jan Kuri <jkuri88@gmail.com>
2020-06-16 10:11:26 +0200

    chore(): init commit

---
+++ projects/ngx-date/src/lib/ngx-date.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxDateComponent } from './ngx-date.component';
+
+
+
+@NgModule({
+  declarations: [NgxDateComponent],
+  imports: [
+  ],
+  exports: [NgxDateComponent]
+})
+export class NgxDateModule { }

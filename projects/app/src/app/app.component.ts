259de76cf5f8e51846bcda224d69c948e89a5841
Jan Kuri <jkuri88@gmail.com>
2020-07-17 07:40:02 +0200

    feat(date-range-picker): init demo

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,27 +1,46 @@
 import { Component } from '@angular/core';
-import { DatepickerOptions } from 'ngx-date';
+import { DatepickerOptions, DateRangePickerOptions } from 'ngx-date';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   date: Date = new Date();
   options: DatepickerOptions = {
     inputClass: 'input',
     calendarClass: 'datepicker-default',
-    scrollBarColor: '#010001',
+    scrollBarColor: '#010001'
   };
   date2: Date = new Date();
   options2: DatepickerOptions = {
     inputClass: 'input',
     calendarClass: 'datepicker-blue',
-    scrollBarColor: '#ffffff',
+    scrollBarColor: '#ffffff'
   };
   date3: Date = new Date();
   options3: DatepickerOptions = {
     inputClass: 'input',
     calendarClass: 'datepicker-dark',
-    scrollBarColor: '#ffffff',
+    scrollBarColor: '#ffffff'
+  };
+
+  dateRange: string;
+  dateRangeOptions: DateRangePickerOptions = {
+    inputClass: 'input',
+    calendarClass: 'daterangepicker-default',
+    scrollBarColor: '#010001'
+  };
+  dateRange2: string;
+  dateRangeOptions2: DateRangePickerOptions = {
+    inputClass: 'input',
+    calendarClass: 'daterangepicker-blue',
+    scrollBarColor: '#010001'
+  };
+  dateRange3: string;
+  dateRangeOptions3: DateRangePickerOptions = {
+    inputClass: 'input',
+    calendarClass: 'daterangepicker-dark',
+    scrollBarColor: '#010001'
   };
 }
--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DatepickerModule } from 'ngx-date';
+import { DatepickerModule, DateRangePickerModule } from 'ngx-date';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, DatepickerModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  imports: [BrowserModule, FormsModule, DatepickerModule, DateRangePickerModule],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
